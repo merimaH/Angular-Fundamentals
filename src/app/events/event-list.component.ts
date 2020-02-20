fb5bc06eeb0d3b372564ab115b6d07bb55b53042
Merima <mhadziabdi1@etf.unsa.ba>
2020-02-20 13:09:46 +0100

    event thumbnail component added

---
+++ src/app/events/event-list.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'event-list',
+  templateUrl: './event-list.component.html',
+  styleUrls: ['./event-list.component.css']
+})
+export class EventListComponent {
+  event1 = {
+    id: 1,
+    name: 'Angular Connect',
+    date: '9/26/2036',
+    time: '10:00 am',
+    price: 599.99,
+    imageUrl: '/assets/images/angularconnect.shield.png',
+    location: {
+      adress: '1057 DT',
+      city: 'London',
+      country: 'England'
+    }
+  };
+}
---
+++ src/app/nav/navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nav-bar',
+  templateUrl: 'navbar.component.html',
+  styleUrls: ['navbar.component.css']
+})
+
+export class NavBarComponent {
+
+}
